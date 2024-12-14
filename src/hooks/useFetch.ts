17b2facedb3f6c22cbddc21560420128a1b66241
Fares Ben khalifa <fbenkhalifa78@gmail.com>
2024-12-14 22:44:40 +0100

    modified

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -9,7 +9,6 @@ export const useFetch = (url: string) => {
   const [data, setData] = useState<PokemonApiResponse | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<Error | null>(null);
-
   useEffect(() => {
     axios
       .get(url)
